fce8134cecc175d3dac5ea3cd61b78696aeb6a77
Raphaël Aurières <raphael.aurieres@gmail.com>
2018-12-19 21:13:46 +0100

    Reset version.

--- src/Application.php
+++ src/Application.php
@@ -13,7 +13,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Application extends SymfonyConsoleApplication
 {
-    const VERSION = 'v4.2.0';
+    const VERSION = 'v1.0.0';
     const MAINTAINER_NAME = 'Raphaël Aurières';
     const MAINTAINER_MAIL = 'raphael.aurieres@gmail.com';
 
