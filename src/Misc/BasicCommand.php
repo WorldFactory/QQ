56cff2c0c37f46155648800ee126bb8a6b947be4
Raphaël Aurières <raphael.aurieres@gmail.com>
2019-01-27 00:30:07 +0100

    Fix docblock.

--- src/Misc/BasicCommand.php
+++ src/Misc/BasicCommand.php
@@ -43,9 +43,8 @@ class BasicCommand extends Command implements ContainerAwareInterface
     private $container;
 
     /**
-     * @param string|null $name The name of the command; passing null means it must be set in configure()
-     *
-     * @throws LogicException When the command name is empty
+     * BasicCommand constructor.
+     * @param array $config
      */
     public function __construct(array $config)
     {
