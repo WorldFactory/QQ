c723ede86ea052e1fabff6a5fb4ebf8a35f9c361
Raphaël Aurières <raphael.aurieres@gmail.com>
2019-01-31 07:38:51 +0100

    Upgrade version number.

--- src/Application.php
+++ src/Application.php
@@ -13,7 +13,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Application extends SymfonyConsoleApplication
 {
-    const VERSION = 'v1.2.9';
+    const VERSION = 'v1.3.0';
     const MAINTAINER_NAME = 'Raphaël Aurières';
     const MAINTAINER_MAIL = 'raphael.aurieres@gmail.com';
 
