2c7adf08603f8e9acceacbc87b4ea38d20b4ec7f
Raphaël Aurières <raphael.aurieres@gmail.com>
2019-02-04 23:43:43 +0100

    Create DeprecationHandler to log deprecations.

---
+++ src/Services/DeprecationHandler.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace WorldFactory\QQ\Services;
+
+
+use Monolog\Handler\AbstractProcessingHandler;
+
+class DeprecationHandler extends AbstractProcessingHandler
+{
+    private $deprecations = [];
+
+    protected function write(array $record)
+    {
+        if (preg_match("/^User Deprecated: (?<text>.*)$/", $record['message'], $matches)) {
+            $this->deprecations[] = $matches['text'];
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function getDeprecations() : array
+    {
+        sort($this->deprecations);
+
+        return array_unique($this->deprecations);
+    }
+}
