bc1e87cf80034235452e079e2f05603a4f61df3e
Raphaël Aurières <raphael.aurieres@gmail.com>
2019-03-09 10:56:59 +0100

    Create contextualized formatter.

---
+++ src/Misc/ContextualizedFormatter.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace WorldFactory\QQ\Misc;
+
+use WorldFactory\QQ\Entities\Context;
+use WorldFactory\QQ\Services\RunnerFactory;
+
+class ContextualizedFormatter
+{
+    /** @var Context */
+    private $context;
+
+    /** @var array */
+    private $tokens = [];
+
+    /** @var array */
+    private $usedArgs = [];
+
+    /**
+     * VarFormatter constructor.
+     * @param ConfigLoader $configLoader
+     */
+    public function __construct(Context $context)
+    {
+        $this->context = $context;
+        $this->tokens = $context->getTokens();
+    }
+
+    public function sanitize(string $var) : string
+    {
+        if (preg_match(RunnerFactory::PROTOCOL_REGEX, $var, $result)) {
+            $header = $result['header'];
+
+            $var = substr($var, strlen($header));
+        }
+
+        return $var;
+    }
+
+    public function format(string $var) : string
+    {
+        $var = $this->injectEnvVars($var);
+        $var = $this->injectParameters($var);
+        $var = $this->injectTokens($var);
+
+        return $var;
+    }
+
+    public function finalize(string $var) : string
+    {
+        $var = $this->injectAllArguments($var);
+        $var = $this->injectLeftArguments($var);
+
+        return $var;
+    }
+
+    protected function injectTokens($var) : string
+    {
+        foreach ($this->tokens as $i => $arg) {
+            $index = $i + 1;
+            if (preg_match("/%$index%/", $var)) {
+                $var = str_replace('%' . $index . '%', $arg, $var);
+                $this->usedArgs[] = $index;
+            }
+        }
+
+        return $var;
+    }
+
+    protected function injectParameters($var) : string
+    {
+        $parameters = $this->context->getParameters();
+
+        foreach ($parameters as $key => $val) {
+            $var = str_replace('%' . $key . '%', $val, $var);
+        }
+
+        return $var;
+    }
+
+    protected function injectEnvVars($var) : string
+    {
+        foreach ($_ENV as $key => $val) {
+            $var = str_replace('%ENV:' . $key . '%', $val, $var);
+        }
+
+        return $var;
+    }
+
+    protected function injectAllArguments($var) : string
+    {
+        if (preg_match("/%_all%/", $var)) {
+            $var = str_replace('%_all%', implode(' ', $this->tokens), $var);
+        }
+
+        return $var;
+    }
+
+    protected function injectLeftArguments($var) : string
+    {
+        if (preg_match("/%_left%/", $var)) {
+            $leftArgs = [];
+
+            foreach ($this->tokens as $key => $arg) {
+                if (!in_array($key + 1, $this->usedArgs)) {
+                    $leftArgs[] = $arg;
+                }
+            }
+
+            $var = str_replace('%_left%', implode(' ', $leftArgs), $var);
+        }
+
+        return $var;
+    }
+}
