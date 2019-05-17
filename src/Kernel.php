1cdeb5c12e62bae3b8387974e664dbf11a477395
Raphaël Aurières <raphael.aurieres@gmail.com>
2019-05-18 00:43:52 +0200

    Move service configuration files into dedicated folder.

--- src/Kernel.php
+++ src/Kernel.php
@@ -66,12 +66,16 @@ class Kernel extends BaseKernel
         // if you are using symfony/dependency-injection 4.0+ as it's the default behavior
         $container->setParameter('container.autowiring.strict_mode', true);
         $container->setParameter('container.dumper.inline_class_loader', true);
+
         $confDir = $this->getProjectDir().'/config';
 
         $loader->load($confDir.'/{packages}/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{packages}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
+
         $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/{services}/*'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/{services}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
 
         $confDir = getcwd().'/config';
 
