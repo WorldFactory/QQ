c018995dc8b02b241b18b5beafa44bc770ddc51b
Raphaël Aurières <raphael.aurieres@gmail.com>
2019-05-19 14:26:43 +0200

    Rename PhpRunner in PHPRunner.

--- src/Services/Runners/PHPRunner.php
+++ src/Services/Runners/PHPRunner.php
@@ -6,14 +6,14 @@ use Exception;
 use WorldFactory\QQ\Foundations\AbstractRunner;
 use WorldFactory\QQ\Misc\TemporizedExecution;
 
-class PhpRunner extends AbstractRunner
+class PHPRunner extends AbstractRunner
 {
     protected const SHORT_DESCRIPTION = "Execute PHP code with 'eval' function.";
 
     protected const LONG_DESCRIPTION = <<<EOT
 This Runner allows you to execute PHP code with the 'eval' function.
-The context is the 'run' method of the PhpRunner class.
-You have at your disposal a \$script object of type WorldFactory\QQ\Entities\Script, as well as all the protected and private methods of the PhpRunner.
+The context is the 'run' method of the PHPRunner class.
+You have at your disposal a \$script object of type WorldFactory\QQ\Entities\Script, as well as all the protected and private methods of the PHPRunner.
 EOT;
 
     /**
