8a23957d172323868cb3a545ee18a259bcb9088f
Raphaël Aurières <raphael.aurieres@gmail.com>
2019-05-26 16:57:53 +0200

    Create ExpressionRunner.

---
+++ src/Services/Runners/ExpressionRunner.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace WorldFactory\QQ\Services\Runners;
+
+use Exception;
+use WorldFactory\QQ\Misc\TemporizedExecution;
+
+class ExpressionRunner extends PHPRunner
+{
+    protected const SHORT_DESCRIPTION = "Execute PHP expression with 'eval' function and return it's result.";
+
+    protected const LONG_DESCRIPTION = <<<EOT
+This Runner allows you to execute PHP code with the 'eval' function.
+Before it runs, your code is encapsulated as follows: "return (<your_code>);"
+The context is the 'execute' method of the ExpressionRunner class.
+You have at your disposal all parameters extracted in the current symbol table, as well as all the protected and private methods of the ExpressionRunner.
+EOT;
+
+    public function format(string $compiledScript): string
+    {
+        return "return ($compiledScript);";
+    }
+
+    /**
+     * @inheritdoc
+     * @throws Exception
+     */
+    public function execute(string $script)
+    {
+        $_parameters = $this->getContext()->getParameters();
+
+        extract($_parameters);
+
+        $result = eval($script);
+
+        return $result;
+    }
+}
