a9c0254d2cf17823ec511fdd3c6576317db776f6
Raphaël Aurières <raphael.aurieres@gmail.com>
2019-07-03 13:02:16 +0200

    Upgrade version 1.6.7 -> 1.6.8

--- src/Application.php
+++ src/Application.php
@@ -13,7 +13,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Application extends SymfonyConsoleApplication
 {
-    const VERSION = 'v1.6.7';
+    const VERSION = 'v1.6.8';
     const MAINTAINER_NAME = 'Raphaël Aurières';
     const MAINTAINER_MAIL = 'raphael.aurieres@gmail.com';
 
